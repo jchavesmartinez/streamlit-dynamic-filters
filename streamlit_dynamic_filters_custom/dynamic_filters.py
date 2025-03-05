a150cfcfded086da5138776633eb9a15fbd83ba5
jchavesmartinez <110587345+jchavesmartinez@users.noreply.github.com>
2025-03-05 02:08:44 -0600

    a

--- streamlit_dynamic_filters_custom/dynamic_filters.py
+++ streamlit_dynamic_filters_custom/dynamic_filters.py
@@ -181,11 +181,9 @@ class DynamicFilters:
                                               key=self.filters_name + filter_name)
             elif location == 'columns' and num_columns > 0:
                 with col_list[counter - 1]:
-                    #selected = st.multiselect(f"Select {self.filters_name + filter_name}", sorted(options),
-                    #                          default=st.session_state[self.filters_name][filter_name],
-                    #                          key=self.filters_name + filter_name)
-                    st.write(self.filters_name + filter_name)
-
+                    selected = st.multiselect(f"Select {self.filters_name + filter_name}", sorted(options),
+                                              default=st.session_state[self.filters_name][filter_name],
+                                              key=self.filters_name + filter_name)
 
                 # increase counter and reset to 1 if max_value is reached
                 counter += 1
